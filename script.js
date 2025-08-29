a8d8c3da650be017b10d88431c85fc0c63795811
Galib76 <galib.hasan.a76@gmail.com>
2025-08-29 18:37:29 +0600

    final check 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let heartClicked = 0;
 document.getElementById('heartCount').innerText = heartClicked;
-let coins = 100;
+let coins = 1000;
 document.getElementById('coinCount').innerText = coins;
 let copies = 0;
 document.getElementById('copyCount').innerText = copies;
